361a2cf3e3d8276bb8efb7eaaa93eb4b5019f2b0
tomazpantadasilvaneto <tomazpanta@hotmail.com>
2024-10-08 21:00:03 -0300

    Aula 6 08-10

---
+++ src/main/java/br/com/alunoonline/api/model/Disciplina.java
@@ -0,0 +1,25 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Disciplina implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+}
