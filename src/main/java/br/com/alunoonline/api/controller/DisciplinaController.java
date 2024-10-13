e6d4b379411d82f96194c80730deafb36d89ab36
tomazpantadasilvaneto <tomazpanta@hotmail.com>
2024-10-13 01:05:34 -0300

    Crud Completo Professor e Disciplina

--- src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
+++ src/main/java/br/com/alunoonline/api/controller/DisciplinaController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/disciplinas")
 
@@ -19,6 +22,21 @@ public class DisciplinaController {
     public void criarDisciplina(@RequestBody Disciplina disciplina) {
         disciplinaService.criarDisciplina(disciplina);
     }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Disciplina> listarTodasDisciplinas() { return disciplinaService.listarTodasDisciplinas();}
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Disciplina> buscarDisciplinaPorId(@PathVariable Long id) { return disciplinaService.buscarDisciplinaPorId(id);}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void detelarDisciplinaPorId(@PathVariable Long id) { disciplinaService.deletarDisciplinaPorId(id);}
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarDisciplinaPorId(@PathVariable Long id, @RequestBody Disciplina disciplina){
+        disciplinaService.atualizarDisciplinaPorId(id, disciplina);
+    }
+
 }
 
 //Teste
--- src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -1,9 +1,15 @@
 package br.com.alunoonline.api.service;
 
+import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.model.Disciplina;
 import br.com.alunoonline.api.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisciplinaService {
@@ -15,4 +21,39 @@ public class DisciplinaService {
         disciplinaRepository.save(disciplina);
     }
 
+    public List<Disciplina> listarTodasDisciplinas() {
+        return disciplinaRepository.findAll();
+    }
+
+    public Optional<Disciplina> buscarDisciplinaPorId(Long id) {
+        return disciplinaRepository.findById(id);
+    }
+
+    public void deletarDisciplinaPorId(Long id) {
+        disciplinaRepository.deleteById(id);
+    }
+
+    public void atualizarDisciplinaPorId(Long id, Disciplina disciplina) {
+        // PRIMEIRO PASSO: VER SE O DISCIPLINA EXISTE NO BD
+        Optional<Disciplina> disciplinaDoBancoDeDados = buscarDisciplinaPorId(id);
+
+        // E SE NÃO EXISTIR???
+        if (disciplinaDoBancoDeDados.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina não encontrada no banco de dados");
+        }
+
+        // SE CHEGAR AQUI, SIGNIFICA QUE EXISTE DISCIPLINA! ENTÃO
+        // VOU ARMAZENA-LO EM UMA VARIÁVEL
+        Disciplina disciplinaEditada = disciplinaDoBancoDeDados.get();
+
+        // COM ESSE DISCIPLINA EITADA DE CIMA, FAÇO
+        // OS SETS NECESSÁRIOS PARA ATUALIZAR OS ATRIBUTOS DELA.
+        disciplinaEditada.setNome(disciplina.getNome());
+
+        // COM O ALUNO TOTALMENTE EDITADO ACIMA
+        // EU DEVOLVO ELE EDITADO/ATUALIZADO PARA O BANCO DE DADOS
+        disciplinaRepository.save(disciplinaEditada);
+
+
+    }
 }
